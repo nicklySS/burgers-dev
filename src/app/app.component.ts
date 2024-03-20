6f59df8f1682a426191f3eae102682798238c0b5
Nick <119859245+nicklySS@users.noreply.github.com>
2024-03-20 18:00:37 +0300

    Add new functional to product get order button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -119,8 +119,11 @@ export class AppComponent {
   constructor(private fb: FormBuilder) {
   }
 
-  scrollTo(target: HTMLElement) {
+  scrollTo(target: HTMLElement, burger?: any) {
     target.scrollIntoView({behavior: "smooth"});
+    if (burger) {
+      this.form.patchValue({order: burger.title + ' (' + burger.price + ' ' + this.currency + ')'});
+    }
   }
 
   confirmOrder(event: Event) {
